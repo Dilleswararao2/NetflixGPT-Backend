e05c3ca0a02bcf7bc192e6ca75d281a99d5cd129
Dilleswararao2 <kadambaladilleswararao.army@gmail.com>
2025-10-26 14:11:37 +0530

    cohere api update

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = 8000;
 
 // Replace this with your actual Cohere API key
-const COHERE_API_KEY = process.env.REACT_APP_COHERE_API_KEY;
+const COHERE_API_KEY = "dompVxSWAr7WPHWZW705B6t3qwibZJMGDCmCDGxc";
 
 app.use(cors());
 app.use(express.json());
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
 app.post("/cohere", async (req, res) => {
   try {
     const cohereRes = await axios.post(
-      "https://api.cohere.ai/v1/generate",
+      "https://api.cohere.ai/v2/chat",
       req.body,
       {
         headers: {
